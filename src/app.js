a061bf51a5969de41ff2f82fbdbe827af75181b3
shrashti-19 <shrashtidwivedi91@gmail.com>
2026-01-17 18:36:14 +0530

    Registered auth routes in Express app under /api/auth.

--- src/app.js
+++ src/app.js
@@ -1,7 +1,10 @@
 const express = require("express");
-
+const authRoutes = require("./routes/authRoute");
 const app = express();
 
 app.use(express.json());
 
+
+//auth route for signuo
+app.use("/api/auth", authRoutes);
 module.exports = app;
