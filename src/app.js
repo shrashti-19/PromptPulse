d4fb640c08cb591dcfeadcc75917c5e94a0f26ea
shrashti-19 <shrashtidwivedi91@gmail.com>
2026-01-24 23:18:11 +0530

    created content create api

--- src/app.js
+++ src/app.js
@@ -1,6 +1,10 @@
 const express = require("express");
 const authRoutes = require("./routes/authRoute");
 const testRoutes = require("./routes/testRoutes");
+const contentRoutes = require("./routes/contentRoutes");
+
+
+
 const app = express();
 
 app.use(express.json());
@@ -9,5 +13,7 @@ app.use(express.json());
 //auth route for signuo
 app.use("/api/auth", authRoutes);
 app.use("/api/test", testRoutes);
+app.use("/api/content", contentRoutes);
+
 
 module.exports = app;
