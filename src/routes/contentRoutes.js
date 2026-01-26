6a4227e0514fff1f5992158c32a6f6a5735192e0
shrashti-19 <shrashtidwivedi91@gmail.com>
2026-01-26 18:03:10 +0530

    Added protected GET content route to fetch only the logged-in user's data.

--- src/routes/contentRoutes.js
+++ src/routes/contentRoutes.js
@@ -1,10 +1,15 @@
 const express = require("express");
-const { createContent } = require("../controllers/contentController");
+const { createContent, getMyContent } = require("../controllers/contentController");
 const authMiddleware = require("../middleware/authMiddleware");
 
 const router = express.Router();
 
 // create content (protected)
 router.post("/", authMiddleware, createContent);
+
+//get my content
+router.get("/", authMiddleware, getMyContent);
+//returns only logged in user's content
+
 
 module.exports = router;
