1439f4418802a2d7ac9c34446e30b786668dc052
shrashti-19 <shrashtidwivedi91@gmail.com>
2026-02-08 21:37:32 +0530

    updated

--- src/routes/authRoute.js
+++ src/routes/authRoute.js
@@ -1,9 +1,10 @@
 const express = require("express");
 const { signup, login } = require("../controllers/authController");
+const authLimiter = require("../middleware/rateLimiter");
 
 const router = express.Router();
 
-router.post("/signup", signup);
-router.post("/login", login);
+router.post("/signup",authLimiter, signup);
+router.post("/login", authLimiter, login);
 
 module.exports = router;
