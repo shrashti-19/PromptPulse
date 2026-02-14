39537d05bad30993f853550ef097ab72b28215ca
shrashti-19 <shrashtidwivedi91@gmail.com>
2026-02-14 23:50:08 +0530

    refresh token for generating new access token route added

--- src/routes/authRoute.js
+++ src/routes/authRoute.js
@@ -1,10 +1,12 @@
 const express = require("express");
-const { signup, login } = require("../controllers/authController");
+const { signup, login, refreshAccessToken } = require("../controllers/authController");
 const authLimiter = require("../middleware/rateLimiter");
 
 const router = express.Router();
 
 router.post("/signup",authLimiter, signup);
 router.post("/login", authLimiter, login);
+router.post("/refresh", refreshAccessToken);
+
 
 module.exports = router;
