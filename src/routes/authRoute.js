8fa4f3c248b705b59680bab6beafb5e9c039e740
shrashti-19 <shrashtidwivedi91@gmail.com>
2026-02-16 21:32:11 +0530

    logout route added

--- src/routes/authRoute.js
+++ src/routes/authRoute.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const { signup, login, refreshAccessToken } = require("../controllers/authController");
+const { signup, login, refreshAccessToken, logout} = require("../controllers/authController");
 const authLimiter = require("../middleware/rateLimiter");
 
 const router = express.Router();
@@ -7,6 +7,6 @@ const router = express.Router();
 router.post("/signup",authLimiter, signup);
 router.post("/login", authLimiter, login);
 router.post("/refresh", refreshAccessToken);
-
+router.post("/logout", logout);
 
 module.exports = router;
